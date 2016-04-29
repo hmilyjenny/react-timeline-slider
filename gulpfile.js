6c2a19d9ab8643f58afbb0ab4ba76e1b20da1632
Mateus "Doodad" Medeiros <dood.ad@outlook.com>
2016-04-29 01:52:13 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,13 @@
 var gulp = require('gulp');
-var sourcemaps = require('gulp-sourcemaps');
 var babel = require('gulp-babel');
 var browserify = require('browserify');
-var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var concat = require('gulp-concat');
+var filter = require('gulp-filter');
+var rename = require('gulp-rename');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 var paths = {
   javascripts: ['lib/**/*.jsx', 'lib/**/*.js'],
@@ -11,12 +15,17 @@ var paths = {
 
 gulp.task('build', function () {
   browserify({
-    entries: 'lib/index',
+    entries: ['lib/index', 'example/browserify/react'],
     extensions: ['.jsx', '.js'],
-    debug: true
+    debug: true,
+    standalone: 'ReactTimelineSlider'
+  })
+  .transform('babelify', { 
+    presets: ['es2015', 'react'],
+    plugins: ['add-module-exports']
   })
   .bundle()
-  .pipe(source('react-timeline-slider.js'))
+  .pipe(source('bundle.js'))
   .pipe(gulp.dest('build'));
 });
 
@@ -28,25 +37,27 @@ gulp.task('publish:npm', function() {
   gulp.src(['lib/**/*.js', 'lib/**/*.jsx'])
     .pipe(sourcemaps.init())
     .pipe(babel({
-      presets: ['es2015', 'react']
+      presets: ['es2015', 'react'],
+      plugins: ['add-module-exports']
     }))
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('dist-npm'));
 });
 
 gulp.task('publish:bundle', function() {
-  browserify({
-    entries: 'lib/index',
-    extensions: ['.jsx', '.js'],
-    debug: false
-  })
-  .external('react')
-  .external('react-dom')
-  .external('underscore')
-  .external('d3')
-  .bundle()
-  .pipe(source('react-timeline-slider.js'))
-  .pipe(gulp.dest('dist'));
+  var indexFilter = filter(['*/index.js'], { restore: true });
+
+  gulp.src(['lib/**/*.js', 'lib/**/*.jsx'])
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['es2015', 'react'],
+      plugins: ['add-module-exports', 'transform-es2015-modules-amd']
+    }))
+    .pipe(indexFilter)
+    .pipe(rename({ basename: 'main' }))
+    .pipe(indexFilter.restore)
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/react-timeline-slider'));
 });
 
 gulp.task('publish', ['publish:npm', 'publish:bundle']);
